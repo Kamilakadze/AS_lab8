7bfac7ff82ffc571d9b1f4d4b50e722e013c6b8d
kamila <dosymbekkamila@gmail.com>
2025-04-06 20:39:33 +0500

    Added ForegroundService in MainActivity2

--- app/src/main/java/com/example/lab8/MainActivity2.java
+++ app/src/main/java/com/example/lab8/MainActivity2.java
@@ -1,13 +1,32 @@
-package com.example.lab8;
+package com.example.lab08;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.lab8.R;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private Intent foregroundServiceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        foregroundServiceIntent = new Intent(this, MyService.class);
+
+        Button startBtn = findViewById(R.id.button_start_music);
+        Button stopBtn = findViewById(R.id.button_stop_music);
+
+        startBtn.setOnClickListener(v ->
+                ContextCompat.startForegroundService(MainActivity2.this, foregroundServiceIntent));
+
+        stopBtn.setOnClickListener(v -> stopService(foregroundServiceIntent));
     }
 }
