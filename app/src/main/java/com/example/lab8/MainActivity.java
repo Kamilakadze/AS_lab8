d193ee7f041b9627ae4347fd164d6064847b81bc
kamila <dosymbekkamila@gmail.com>
2025-04-06 20:25:17 +0500

    Adding main activity

--- app/src/main/java/com/example/lab8/MainActivity.java
+++ app/src/main/java/com/example/lab8/MainActivity.java
@@ -1,24 +1,61 @@
-package com.example.lab8;
+package com.example.lab08;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.lab8.R;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText randomCharEditText;
+    private Intent backgroundServiceIntent;
+    private BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        randomCharEditText = findViewById(R.id.editText_randomChar);
+        backgroundServiceIntent = new Intent(this, RandomCharacterService.class);
+
+        Button startBtn = findViewById(R.id.button_start);
+        Button stopBtn = findViewById(R.id.button_stop);
+        Button nextBtn = findViewById(R.id.button_next);
+
+        startBtn.setOnClickListener(v -> startService(backgroundServiceIntent));
+        stopBtn.setOnClickListener(v -> {
+            stopService(backgroundServiceIntent);
+            randomCharEditText.setText("");
         });
+        nextBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MainActivity2.class)));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter("my.custom.action.tag.lab6");
+        broadcastReceiver = new MyReceiver();
+        registerReceiver(broadcastReceiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    public class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            char c = intent.getCharExtra("randomCharacter", '?');
+            randomCharEditText.setText(String.valueOf(c));
+        }
     }
 }
